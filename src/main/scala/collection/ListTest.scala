d4bdf88f91612cc61651e6ecf07a3159e3b7bfd9
juguanghui <juguanghui@megvii.com>
2018-01-29 18:10:39 +0800

    Collection Test

--- src/main/scala/collection/ListTest.scala
+++ src/main/scala/collection/ListTest.scala
@@ -1,12 +1,29 @@
 package collection
+
+
+
 case class DMPoint1(x: Integer, y: Integer, var stay_seconds: Integer, sku_id: String, sku_action: String)
+case class DM(x: Integer, y: Integer, var stay_seconds: Integer, list: List[Map[String,String]])
 object ListTest extends App {
   val site: List[DMPoint1] = List(DMPoint1(2,3,3,"1","3"), DMPoint1(2,3,3,"1","3"),DMPoint1(2,4,3,"1","3"),DMPoint1(2,4,3,"1","3"))
-  val groupByXY = site.groupBy(item => (item.x,item.y))
+  val groupByXY = site.groupBy(item => (item.x, item.y))
   println(groupByXY)
 
-  val sum = groupByXY.mapValues(_.reduce((a,b) => new DMPoint1(a.x, a.y, a.stay_seconds + b.stay_seconds, a.sku_id, a.sku_action)))
-  println(sum)
+  val sum = groupByXY.mapValues(_.reduce((a,b) => DMPoint1(a.x, a.y, a.stay_seconds + b.stay_seconds, a.sku_id,a.sku_action)))
+  println(sum.values.toList)
+
+/*  val sum = groupByXY.mapValues(_.reduce((a,b) => {
+    var map = Map(a.sku_id -> a.sku_action,
+                   b.sku_id -> b.sku_action)
+    var list = List[Map[String,String]]()
+    var dm = DM
+    list = list :+ map
+    dm.apply(a.x, a.y, a.stay_seconds, list)
+    dm
+  })*/
+
+//  sum.values.toList
+//  println(sum)
 }
 
 
---
+++ src/main/scala/collection/MapTest.scala
@@ -0,0 +1,21 @@
+package collection
+
+object MapTest {
+   def main(args: Array[String]) {
+      val colors1 = Map("red" -> "#FF0000",
+                        "azure" -> "#F0FFFF",
+                        "peru" -> "#CD853F")
+      val colors2 = Map("blue" -> "#0033FF",
+                        "yellow" -> "#FFFF00",
+                        "red" -> "#FF0001")
+
+      //  ++ 作为运算符
+      var colors = colors1 ++ colors2
+      println( "colors1 ++ colors2 : " + colors )
+
+      //  ++ 作为方法
+      colors = colors1.++(colors2)
+      println( "colors1.++(colors2)) : " + colors )
+
+   }
+}
