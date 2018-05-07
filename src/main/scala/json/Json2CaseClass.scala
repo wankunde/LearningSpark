3c56dd2c89c83b99fb9b3de6d563065279ca2a0e
juguanghui <juguanghui@megvii.com>
2018-05-07 12:51:37 +0800

    json 2 case class demo

--- src/main/scala/json/Json2CaseClass.scala
+++ src/main/scala/json/Json2CaseClass.scala
@@ -1,28 +1,17 @@
 package json
 
 import org.json4s.native.Serialization.{read, write}
-/***
-  * http://json4s.org/
-  */
+
 object Json2CaseClass extends App {
   implicit val formats = org.json4s.DefaultFormats
 
-  case class SkuInfo(sku_id: String, sku_action: Integer) // 字段名称要与json的key值相同
-  case class IOTPoint(x: Integer, y: Integer, enter_time: Integer, leave_time: Integer, sku_list: List[SkuInfo])
-  case class DMPoint(x: Integer, y: Integer, var stay_seconds: Integer, sku_list: List[SkuInfo])
-  case class IOTBehaviorTrace(gateway_id: String, face_id: String, trace_id:String, start_time: Integer, end_time: Integer, data: List[IOTPoint])
-  case class DMBehaviorTrace(request_id: String, gateway_id: String, face_id: String, trace_id:String, start_time: Integer, end_time: Integer, data: List[DMPoint])
-  val s = "{\"gateway_id\":\"44488b839a0b4661a9d77c4ac1de570e\",\"face_id\":\"\",\"trace_id\":\"98fb40e306cd44f7a9f4b5977d1852d7\",\"start_time\":1517274076,\"end_time\":1517274094," +
-    "\"data\":[{\"x\":821,\"y\":219,\"enter_time\":1517274076,\"leave_time\":1517274077," +
-    "\"sku_list\":[{\"sku_id\":\"af44603207f946d8b34327fc7ea5871f\",\"sku_action\":0}," +
-    "{\"sku_id\":\"6051ce69fcb6435f85bc98cd42ba6505\",\"sku_action\":2}," +
-    "{\"sku_id\":\"095cceae0a77498d9bfdce20a938daca\",\"sku_action\":2}," +
-    "{\"sku_id\":\"18b110a16aa44aefadc0c4d20ec6133f\",\"sku_action\":0}," +
-    "{\"sku_id\":\"7a478b19f10b489c9bb3a7b3d14346b0\",\"sku_action\":0}]}]}"
+  case class Class(_name:String, students: List[Student])
+  case class Student(sid:String, _name:String)
 
-//  val iotBehaviorTrace:IOTBehaviorTrace = parse(s).extract[IOTBehaviorTrace]
-  val iotBehaviorTrace:IOTBehaviorTrace =read[IOTBehaviorTrace](s)
-  println(iotBehaviorTrace)
+  val s = "{\"_name\":\"Class1\",\"students\":[{\"sid\":\"1\",\"_name\":\"小明\"},{\"sid\":\"1\",\"_name\":\"小王\"}]}"
 
-  println(write(iotBehaviorTrace))
+  val clazz:Class = read[Class](s)
+  println(clazz)
+
+  println(write(clazz))
 }
